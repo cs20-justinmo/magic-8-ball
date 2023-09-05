3239a80805e5ed3976316e6534b2eb08f5d71cff
justinmo128 <j.mo1@share.epsb.ca>
2023-09-05 14:52:18 -0600

    finish the assignment for real

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-document.getElementById("eightball").addEventListener("click", getResponse);
+document.getElementById("eightball").addEventListener("click", shake8Ball);
 
-let output = document.getElementById("eightout");
+function shake8Ball() {
+    document.getElementById("eightout").innerHTML = getResponse();
+}
 
 function getResponse() {
     let input = document.getElementById("eighttext").value.toLowerCase();
-    if (input == "") {
-        output.innerHTML = "Please ask a question...";
-    } else if (input == "does a magic 8 ball actually work?") {
-        output.innerHTML = "How dare you doubt me!";
-    } else if (input == "is javascript awesome?") {
-        output.innerHTML = "Of Course!";
-    } else {
-        output.innerHTML = randomResponse();
-    }
+    if (input == "") return "Please ask a question...";
+    else if (input == "does a magic 8 ball actually work?") return "How dare you doubt me!";
+    else if (input == "is javascript awesome?") return "Of Course!";
+    else return randomResponse();
 }
 
 function randomResponse() {
     let randNum = Math.random();
-    console.log(randNum)
     if (randNum < 0.2) return "Without a Doubt.";
     else if (randNum < 0.4) return "As I see it, yes.";
     else if (randNum < 0.6) return "Concentrate and ask again.";
